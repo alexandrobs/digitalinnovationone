17d3bebd65f1039a41d39dc0cfeb349d6743046e
alexandrobs <jornalsinal@gmail.com>
2022-07-06 23:36:38 -0300

    modify class Pessoa

--- src/main/kotlin/one/digitalinnovation/digitalbank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digitalbank/Pessoa.kt
@@ -1,4 +1,14 @@
 package one.digitalinnovation.digitalbank
 
 class Pessoa {
+
+    var nome:String = "Alex"
+    var cpf:String = "339.150.308-48"
+}
+
+fun main(){
+    val alex = Pessoa()
+
+    println(alex.nome)
+    println(alex.cpf)
 }
