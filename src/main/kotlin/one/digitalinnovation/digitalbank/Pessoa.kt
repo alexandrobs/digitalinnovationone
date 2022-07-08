64f8223c56e5f158c868aa3c582c981e1e797206
alexandrobs <jornalsinal@gmail.com>
2022-07-07 23:46:36 -0300

    modify class Pessoa, private set change

--- src/main/kotlin/one/digitalinnovation/digitalbank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digitalbank/Pessoa.kt
@@ -3,12 +3,33 @@ package one.digitalinnovation.digitalbank
 class Pessoa {
 
     var nome:String = "Alex"
+
+    //exemplo para deixar set private
     var cpf:String = "339.150.308-48"
+    private set
+    //
+
+    //exemplo modificador de acesso private, sem get e sem set
+    //private var cpf:String = "339.150.308-48"
+
+    //
+
+    //exemplo de inner class
+//    inner class Endereco{
+//        var rua:String = "rua vila lobos"
+//    }
 }
 
 fun main(){
     val alex = Pessoa()
 
     println(alex.nome)
+
+    //quando o set ta privado, isso da erro, porque apenas o get ta publico
+    //alex.cpf = "123456"
+
     println(alex.cpf)
+
+    //exemplo chamando membro inner class
+    //println(alex.Endereco().rua)
 }
