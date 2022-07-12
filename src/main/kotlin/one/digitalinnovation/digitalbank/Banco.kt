4fcfc11545bbdf82e6df1d0fac1778c84748c064
alexandrobs <jornalsinal@gmail.com>
2022-07-12 19:37:58 -0300

    data class in Banco and BancoTeste

--- src/main/kotlin/one/digitalinnovation/digitalbank/Banco.kt
+++ src/main/kotlin/one/digitalinnovation/digitalbank/Banco.kt
@@ -3,4 +3,6 @@ package one.digitalinnovation.digitalbank
 data class Banco(
     val nome: String,
     val numero: Int
-)
+) {
+    fun info() = "Nome do banco: $nome, numero da conta: $numero"
+}
--- src/main/kotlin/one/digitalinnovation/digitalbank/testes/BancoTeste.kt
+++ src/main/kotlin/one/digitalinnovation/digitalbank/testes/BancoTeste.kt
@@ -1,5 +1,23 @@
 package one.digitalinnovation.digitalbank.testes
 
+import one.digitalinnovation.digitalbank.Banco
+
 fun main(){
+
+    //com assinatura de parametros no construtor
+    //val digioneBank = Banco(nome = "DigiOneBank", numero = 1234567890)
+    //sem assinatura de parametros no construtor
+    val digioneBank = Banco("DigiOneBank", 1234567890)
+
+    println(digioneBank.nome)
+    println(digioneBank.numero)
+
+    val banco2 = digioneBank.copy("Banco Pirata", 1234567890)
+
+    println(banco2.nome)
+    println(banco2.numero)
+
+    val banco3 = Banco("Banco do Brasil", 2022002255)
+    println(banco3.info())
 
 }
